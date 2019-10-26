524cc08b350f727c8cec0e1767d12fb6a58cf354
Ivan <ivan.trajchev@symphony-solutions.eu>
2019-10-26 15:25:02 +0200

    Edited User Model

--- server/models/user/user.model.js
+++ server/models/user/user.model.js
@@ -13,16 +13,59 @@ const User = sequelize.define( 'user', {
         username: {
             type: Sequilize.STRING,
             allowNull: false,
-            unique: true
+            unique: true,
+            validate: {
+                len: [4, 24],
+                isAlphaNumeric: true
+            }
         },
         email: {
             type: Sequilize.STRING,
             allowNull: false,
-            unique: true
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        photo: {
+            type: Sequilize.STRING,
+            allowNull: false,
+            defaultValue: 'image.jpg'
+        },
+        role: {
+            type: Sequilize.STRING,
+            allowNull: false,
+            validate: {
+                isIn: [['beginner', 'advanced', 'pro', 'admin']]
+            },
+            defaultValue: 'beginner'
         },
         password: {
             type: Sequilize.STRING,
             allowNull: false,
+            validate: {
+                len: [8, 24]
+            }
+        },
+        passwordConfirm: {
+            type: Sequilize.STRING,
+            allowNull: false,
+            validate: {
+                equals: this.password
+            }
+        },
+        passwordResetToken: {
+            type: Sequilize.STRING
+        },
+        passwordResetToken: {
+            type: Sequilize.STRING
+        },
+        passwordResetExpires: {
+            type: Sequilize.DATE
+        },
+        active: {
+            type: Sequilize.BOOLEAN,
+            defaultValue: true
         }
     }     
 );
---
+++ server/utils/ApiFeatures.js
@@ -0,0 +1,12 @@
+class APIFeatures {
+
+    constructor(query, queryString) {
+        this.query = query;
+        this.queryString = queryString;
+    }
+
+    
+
+}
+
+module.exports = APIFeatures;
