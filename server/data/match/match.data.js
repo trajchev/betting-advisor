7f627c9954674198796c287d0ffea378d957d54e
Ivan <ivan.trajchev@symphony-solutions.eu>
2019-10-31 16:08:53 +0100

    Added get data from api

---
+++ server/data/match/match.data.js
@@ -0,0 +1,55 @@
+const axios = require('axios');
+
+const models = require('../../models/models');
+
+const Match = models.Match;
+const Odd = models.Odd;
+
+// IMPORT DATA INTO DB
+const getMatchesOdds = (league, region) => {
+    axios.get(`${process.env.API_URL}odds`, {
+        params: {
+            api_key: process.env.API_KEY,
+            sport: league,
+            region: region,
+        }
+    })
+    .then(response => {
+        response.data.data.forEach(dataObj => {
+            // Create the Match and save to db
+            const match = new Match({
+                home_team: dataObj.teams[0],
+                away_team: dataObj.teams[1],
+                commence_time: dataObj.commence_time,
+                sport_key: dataObj.sport_key,
+            });
+
+            return match.save()
+            .then(matchData => {
+                dataObj.sites.forEach(site => {
+                    // Create odd and save to db
+                    const type = Object.keys(site.odds)[0];
+                    const oddsArr = site.odds[type];
+                    const odd = new Odd({
+                        type: type,
+                        home_team: oddsArr[0],
+                        draw: oddsArr[1],
+                        away_team: oddsArr[2],
+                        match_id: matchData.dataValues.id
+                    });
+        
+                    return odd.save()
+                });
+            });
+        });
+    
+    })
+    .catch(err => {
+        console.log('Error status', err.response.status);
+        console.log(err.response.data);
+    });
+    
+    process.exit();
+}
+
+module.exports = getMatchesOdds;
---
+++ server/data/sport/sport.data.js
@@ -0,0 +1,53 @@
+const axios = require('axios');
+
+const models = require('../../models/models');
+
+const Sport = models.Sport;
+
+// IMPORT Sports data into DB
+const importSports = async () => {
+    try {
+
+        axios.get(`${process.env.API_URL}sports`, {
+            params: {
+                api_key: process.env.API_KEY
+            }
+        })
+        .then(response => {
+            response.data.data.forEach(dataObj => {
+                Sport.findOne({where: {key: dataObj.key}})
+                .then(sportResult => {
+                    if (!sportResult) {
+    
+                        const sport = new Sport({
+                            key: dataObj.key,
+                            active: dataObj.active,
+                            group: dataObj.group,
+                            details: dataObj.details,
+                            title: dataObj.title,
+                        });
+    
+                        return sport.save();
+    
+                    }
+                })
+            })
+        
+        })
+        .catch(err => {
+            console.log('Error status', err.response.status);
+            console.log(error.response.data);
+        });
+
+        console.log('Data successfully loaded!');
+
+    } catch (err) {
+
+      console.log(err);
+
+    }
+
+    process.exit();
+};
+
+module.exports = importSports;
