321ebd4a6d832dc32e7176c6c02c9c8213d39a9f
Ivan <ivan.trajchev@symphony-solutions.eu>
2019-11-18 15:57:07 +0100

    Password reset link adjustment

--- server/controllers/auth/authController.js
+++ server/controllers/auth/authController.js
@@ -144,7 +144,7 @@ const forgotPassword = catchAsync(async (req, res, next) => {
     await user.save();
     
     // 3. Send it to user email
-    const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
+    const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/reset-assword/${resetToken}`;
     const message = `Forgot your Password? Submit a patch request with your new password and passwordConfirm to ${resetURL}. \nIf you didn't forget your password, please ignore this email`;
 
     try {
