174b89018e154ee71d76ca8d7d8ae12decd527ec
Ivan <ivan.trajchev@symphony-solutions.eu>
2019-12-25 09:24:13 +0100

    Restructured data pull code from file

--- server/data/match/match.data.js
+++ server/data/match/match.data.js
@@ -4,14 +4,11 @@ const fs = require('fs');
 const path = require('path');
 
 const models = require('../../models/models');
+const saveData = require('../../utils/save2DB');
 
 const Match = models.Match;
-const Odd = models.Odd;
 const Team = models.Team;
 const Site = models.Site;
-const Totals = models.Totals;
-const Spreads = models.Spreads;
-const H2H = models.H2H;
 
 const pullMatchesOdds = (sport, region, oddsType) => {
     axios.get(`${process.env.API_URL}odds`, {
@@ -59,7 +56,7 @@ const pullMatchesOdds = (sport, region, oddsType) => {
 };
 
 const getMatchesOdds = () => {
-    const matchesPath = path.join(__dirname, '/h2hEPL.json')
+    const matchesPath = path.join(__dirname, '/spreadsEPL.json')
 
     const rawMatches = fs.readFileSync(matchesPath, error => {
         if (error) {
@@ -137,69 +134,15 @@ const getMatchesOdds = () => {
 
                         if (type === 'totals') {
 
-                            Totals.findOne({where: {
-                                match_id: matchResult.id,
-                                site_id: siteID
-                            }})
-                            .then(totalsRes => {
-                                if ( !totalsRes ) {
-                                    const total = new Totals({
-                                        type: 'totals',
-                                        position_over: siteObj.odds.totals.position[0],
-                                        position_under: siteObj.odds.totals.position[1],
-                                        odds_home: siteObj.odds.totals.odds[0],
-                                        odds_away: siteObj.odds.totals.odds[1],
-                                        points_home: siteObj.odds.totals.points[0],
-                                        points_away: siteObj.odds.totals.points[1],
-                                        match_id: matchResult.id,
-                                        site_id: siteID
-                                    });
-                    
-                                    return total.save();
-                                }
-                            })
+                            saveData.checkSaveTotals(siteObj, matchResult, siteID);
+
                         } else if (type === 'spreads') {
 
-                            Spreads.findOne({where: {
-                                match_id: matchResult.id,
-                                site_id: siteID
-                            }})
-                            .then(spreadsRes => {
-                                if ( !spreadsRes ) {
-                                    const spread = new Spreads({
-                                        type: 'spreads',
-                                        odds_home: siteObj.odds.spreads.odds[0],
-                                        odds_away: siteObj.odds.spreads.odds[1],
-                                        points_home: siteObj.odds.spreads.points[0] * 1,
-                                        points_away: siteObj.odds.spreads.points[1] * 1,
-                                        match_id: matchResult.id,
-                                        site_id: siteID
-                                    });
-                    
-                                    return spread.save();
-                                }
-                            })
+                            saveData.checkSaveSpreads(siteObj, matchResult, siteID);
 
                         } else if ( type === 'h2h' ) {
 
-                            H2H.findOne({where: {
-                                match_id: matchResult.id,
-                                site_id: siteID
-                            }})
-                            .then(h2hRes => {
-                                if ( !h2hRes ) {
-                                    const h2h = new H2H({
-                                        type: 'h2h',
-                                        odds_home: siteObj.odds.h2h[0],
-                                        odds_draw: siteObj.odds.h2h[1],
-                                        odds_away: siteObj.odds.h2h[2],
-                                        match_id: matchResult.id,
-                                        site_id: siteID
-                                    });
-                    
-                                    return h2h.save();
-                                }
-                            })
+                            saveData.checkSaveH2H(siteObj, matchResult, siteID);
 
                         }
 
@@ -209,69 +152,15 @@ const getMatchesOdds = () => {
                     
                     if (type === 'totals') {
 
-                        Totals.findOne({where: {
-                            match_id: matchResult.id,
-                            site_id: siteID
-                        }})
-                        .then(totalsRes => {
-                            if ( !totalsRes ) {
-                                const total = new Totals({
-                                    type: 'totals',
-                                    position_over: siteObj.odds.totals.position[0],
-                                    position_under: siteObj.odds.totals.position[1],
-                                    odds_home: siteObj.odds.totals.odds[0],
-                                    odds_away: siteObj.odds.totals.odds[1],
-                                    points_home: siteObj.odds.totals.points[0],
-                                    points_away: siteObj.odds.totals.points[1],
-                                    match_id: matchResult.id,
-                                    site_id: siteID
-                                });
-                
-                                return total.save();
-                            }
-                        })
+                        saveData.checkSaveTotals(siteObj, matchResult, siteID);
+
                     } else if (type === 'spreads') {
 
-                        Spreads.findOne({where: {
-                            match_id: matchResult.id,
-                            site_id: siteID
-                        }})
-                        .then(spreadsRes => {
-                            if ( !spreadsRes ) {
-                                const spread = new Spreads({
-                                    type: 'spreads',
-                                    odds_home: siteObj.odds.spreads.odds[0],
-                                    odds_away: siteObj.odds.spreads.odds[1],
-                                    points_home: siteObj.odds.spreads.points[0] * 1,
-                                    points_away: siteObj.odds.spreads.points[1] * 1,
-                                    match_id: matchResult.id,
-                                    site_id: siteID
-                                });
-                
-                                return spread.save();
-                            }
-                        })
+                        saveData.checkSaveSpreads(siteObj, matchResult, siteID);
 
                     } else if ( type === 'h2h' ) {
 
-                        H2H.findOne({where: {
-                            match_id: matchResult.id,
-                            site_id: siteID
-                        }})
-                        .then(h2hRes => {
-                            if ( !h2hRes ) {
-                                const h2h = new H2H({
-                                    type: 'h2h',
-                                    odds_home: siteObj.odds.h2h[0],
-                                    odds_draw: siteObj.odds.h2h[1],
-                                    odds_away: siteObj.odds.h2h[2],
-                                    match_id: matchResult.id,
-                                    site_id: siteID
-                                });
-                
-                                return h2h.save();
-                            }
-                        })
+                        saveData.checkSaveH2H(siteObj, matchResult, siteID);
 
                     }
                 })
@@ -279,8 +168,6 @@ const getMatchesOdds = () => {
                     console.log('Error status', err);
                     return new Error(err);
                 });
-
-                // console.log('SITE ================================= ', siteObj.odds.totals);
 
             })
         })
---
+++ server/utils/save2DB.js
@@ -0,0 +1,92 @@
+const models = require('../models/models');
+
+const Totals = models.Totals;
+const Spreads = models.Spreads;
+const H2H = models.H2H;
+
+const checkSaveTotals = ( siteObj, matchResult, siteID ) => {
+
+    Totals.findOne({where: {
+        match_id: matchResult.id,
+        site_id: siteID
+    }})
+    .then(totalsRes => {
+
+        if ( !totalsRes ) {
+            const total = new Totals({
+                type: 'totals',
+                position_over: siteObj.odds.totals.position[0],
+                position_under: siteObj.odds.totals.position[1],
+                odds_home: siteObj.odds.totals.odds[0],
+                odds_away: siteObj.odds.totals.odds[1],
+                points_home: siteObj.odds.totals.points[0],
+                points_away: siteObj.odds.totals.points[1],
+                match_id: matchResult.id,
+                site_id: siteID
+            });
+    
+            return total.save();
+        }
+
+    })
+
+};
+
+const checkSaveSpreads = ( siteObj, matchResult, siteID ) => {
+
+    Spreads.findOne({where: {
+        match_id: matchResult.id,
+        site_id: siteID
+    }})
+    .then(spreadsRes => {
+
+        if ( !spreadsRes ) {
+            const spread = new Spreads({
+                type: 'spreads',
+                odds_home: siteObj.odds.spreads.odds[0],
+                odds_away: siteObj.odds.spreads.odds[1],
+                points_home: siteObj.odds.spreads.points[0] * 1,
+                points_away: siteObj.odds.spreads.points[1] * 1,
+                match_id: matchResult.id,
+                site_id: siteID
+            });
+    
+            return spread.save();
+        }
+
+    })
+
+}
+
+const checkSaveH2H = ( siteObj, matchResult, siteID) => {
+
+    H2H.findOne({where: {
+        match_id: matchResult.id,
+        site_id: siteID
+    }})
+    .then(h2hRes => {
+
+        if ( !h2hRes ) {
+            const h2h = new H2H({
+                type: 'h2h',
+                odds_home: siteObj.odds.h2h[0],
+                odds_draw: siteObj.odds.h2h[1],
+                odds_away: siteObj.odds.h2h[2],
+                match_id: matchResult.id,
+                site_id: siteID
+            });
+    
+            return h2h.save();
+        }
+
+    })
+
+}
+
+module.exports = {
+
+    checkSaveTotals,
+    checkSaveSpreads,
+    checkSaveH2H
+
+}
